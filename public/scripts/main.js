2c243a5e3fe5781142f3329dc2a117f1194b6a55
KaydeeDee <kd.dangelo@gmail.com>
2018-09-14 20:49:23 -0400

    query changes, google analyics added, fix to nav jumping on opacity, consistancy added to js doc

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,46 +1,49 @@
-'use strict';
+"use strict";
 
 var spencer = {};
 
 spencer.effects = function () {
     // Smooth scroll
-    $('nav a').smoothScroll({
+    $("nav a").smoothScroll({
         speed: 1350,
         offset: 5
     });
 
     // hover and click effect on nav a
     spencer.hoverClick = function () {
-        $('nav a').hover(function () {
-            $('nav a').addClass('navOpacity');
-            $(this).removeClass('navOpacity');
+        $("nav a").hover(function () {
+            $("nav a").addClass("navOpacity");
+            $(this).removeClass("navOpacity");
         }, function () {
-            $('nav a').removeClass('navOpacity');
+            $("nav a").removeClass("navOpacity");
         });
     };
     spencer.hoverClick();
 
     // active states on nav
-
+    //below is a waypoint solution that C.Keeley used. Visit her at www.chelseakeeley.com 
     spencer.scrolling = function () {
         // default home as active
-        $("#toTop").addClass("active-square--nav");
+        $("#toTop--squareActive").addClass("active-square--nav");
 
         $(window).scroll(function () {
             var location = $(this).scrollTop();
 
             // id by section
-            $('section').each(function () {
+            $("section").each(function () {
                 var target = $(this).offset().top;
-                var targetID = $(this).attr('id');
+                var targetID = $(this).attr("id");
 
                 // location vs. target conditional
                 if (location >= target) {
                     $(".inactive-square-transparent--nav").removeClass("active-square--nav");
-                    $('#' + targetID + '--squareActive').addClass("active-square--nav");
-                } else if (location === 0) {
+                    $("#" + targetID + "--squareActive").addClass("active-square--nav");
+                } else if (target - location <= 5) {
                     $(".inactive-square-transparent--nav").removeClass("active-square--nav");
-                    $("#toTop").addClass("active-square--nav");
+                    $("#" + targetID + "--active").addClass("active-square--nav");
+                } else if (location <= 50) {
+                    $(".inactive-square-transparent--nav").removeClass("active-square--nav");
+                    $("#toTop--squareActive").addClass("active-square--nav");
                 }
             });
         });
@@ -50,23 +53,24 @@ spencer.effects = function () {
     // hamberger menu
     //screen size listener
     function screenSize() {
-        if (window.matchMedia('(max-width: 1000px)').matches) {
-            $('.hamburger').show();
-            $('.nav--flex').addClass('nav--mobile').removeClass('nav--flex');
-            $('.anchor-in-text--global').removeClass('tooltip'); //disable tooltip for mobile
+        if (window.matchMedia("(max-width: 1000px)").matches) {
+            $(".hamburger").show();
+            $(".nav--flex").addClass("nav--mobile").removeClass("nav--flex");
+            $(".anchor-in-text--global").removeClass("tooltip"); //disable tooltip for mobile
         } else {
-            $('.nav--mobile').addClass('nav--flex').removeClass('nav--mobile');
-            $('.hamburger').hide();
-            $('nav').removeClass('open'); //if window grows while hamburger is open, close hamburger, reset it to closed by default
-            $('.anchor-in-text--global').addClass('tooltip'); //disable tooltip for mobile
+            $(".nav--mobile").addClass("nav--flex").removeClass("nav--mobile");
+            $(".hamburger").hide();
+            $("nav").removeClass("open"); //if window grows while hamburger is open, close hamburger, reset it to closed by default
+            $(".anchor-in-text--global").addClass('tooltip'); //enable tooltip for larger screens
         }
     };
 
-    window.addEventListener('resize', screenSize, false);
+    window.addEventListener("resize", screenSize, false);
     screenSize();
 
-    $('nav').click(function () {
-        $(this).toggleClass('open');
+    // toggling mobile nav full screen ul
+    $("nav").click(function () {
+        $(this).toggleClass("open");
     });
 };
 
